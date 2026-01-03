a9c00f93dc2034e1c843dc833a2779f2fca267c0
Harikrishna-1971 <harikrishnagarimella@gmail.com>
2026-01-03 19:20:39 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App(){
+  return(
+    <div className="app">
+        <h1>Hello World!</h1>
+        <h2>profile</h2>
+        <p>Name G.harikrishna</p>
+        <p>program:B.Tech-ECE</p>
+        <p>Section:so3</p>
+        <p>Course:Front End Development FrameWork and UI Engineering</p>
+    </div>
+  );
+}
+
+export default App;
